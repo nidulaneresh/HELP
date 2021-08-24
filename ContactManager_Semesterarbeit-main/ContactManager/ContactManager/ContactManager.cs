5f35dc76a33c252eb1fd3137934a6f46dff2fc74
nidulaneresh <75679676+nidulaneresh@users.noreply.github.com>
2021-08-24 21:19:07 +0200

    Add files via upload

---
+++ ContactManager_Semesterarbeit-main/ContactManager/ContactManager/ContactManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManager
+{
+    public class ContactManager
+    {
+        public ContactBook contactBook { get; set; }
+
+        public void StartUp()
+        {
+            //Need Testdata
+            /*
+            Disk disk = new Disk();
+            contactBook = disk.Load();
+            */
+        }
+
+    }
+}
